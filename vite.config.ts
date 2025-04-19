2d0b64023e41f863ff43eb3dcc735a165be58402
Donovan Harrison <donjuan0802@gmail.com>
2025-04-19 17:03:35 -0500

    fix: update base path in Vite configuration to the correct repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [],
-    base: '/marvel-rivals-generator/'
+    base: '/rivals-character-picker/'
 })
